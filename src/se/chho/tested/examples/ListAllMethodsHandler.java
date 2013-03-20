33447a58f8e43ca602eacbfda99a3d3d9db24d20
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-03-20 16:08:00 +0100

    Parsing all methods, separating them between tests and non tests and finds all methods that are being executed by tests

--- src/se/chho/tested/examples/ListAllMethodsHandler.java
+++ src/se/chho/tested/examples/ListAllMethodsHandler.java
@@ -28,16 +28,18 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.FileEditorInput;
 
 public class ListAllMethodsHandler extends AbstractHandler {
-	    
+
 	private static final String JDT_NATURE = "org.eclipse.jdt.core.javanature";
+	private static final String VALID_PATTERN_TESTS = "^test(.*)";
+	
+	List<MethodDeclaration> testMethods = new ArrayList<MethodDeclaration>();
+	List<MethodDeclaration> nonTestMethods = new ArrayList<MethodDeclaration>();
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		System.out.println("---- New command ----");
 		// All projects, both closed and opened
 		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
-		
+
 		// Get current active/selected project name
 		IEditorPart  editorPart =
 				PlatformUI.getWorkbench().
@@ -45,125 +47,128 @@ public class ListAllMethodsHandler extends AbstractHandler {
 				getActivePage().
 				getActiveEditor();
 		String activeProjectName = null;
-		
+
 		if(editorPart  != null)
 		{
 			IFileEditorInput input = 
 					(FileEditorInput)editorPart.getEditorInput();
-		    IFile file = input.getFile();
-		    IProject activeProject = 
-		    		file.getProject();
-		    activeProjectName = 
-		    		activeProject.getName();
+			IFile file = input.getFile();
+			IProject activeProject = 
+					file.getProject();
+			activeProjectName = 
+					activeProject.getName();
 		}
-		
+
 		// Iterate over all projects available and analyse the "active" one.
-	    for (IProject project : projects) {
-	    	try {
-	    		if (project.getName().equalsIgnoreCase(activeProjectName)
-	    				&& project.isNatureEnabled(JDT_NATURE))
-	    		{
-	    			// Go through all methods in classes in the current
-	    			// project.
-	    			analyseMethods(project);
-	    			// TODO: Can I mark all methods in "otherMethods" with a red exclamation mark here?
-	    		}
-	      } catch (CoreException e) {
-	        e.printStackTrace();
-	      }
-	    }
-	    return null;
-	  }
-	
+		for (IProject project : projects) {
+			try {
+				if (project.getName().equalsIgnoreCase(activeProjectName)
+						&& project.isNatureEnabled(JDT_NATURE))
+				{
+					// Go through all methods in classes in the current
+					// project.
+					analyseMethods(project);
+					// TODO: Can I mark all methods in "otherMethods" with a red exclamation mark here?
+				}
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
 	/**
 	 * Analysemethods parses a project creates an AST for each Package Fragment Root.
 	 * @param project
 	 * @throws JavaModelException
 	 */
-	  private void analyseMethods(IProject project) throws JavaModelException { 
-		  
+	private void analyseMethods(IProject project) throws JavaModelException { 
+
 		// Returns all package fragments in all package fragment roots contained in this project
 		IPackageFragment[] packages = JavaCore.create(project).getPackageFragments();
 
-	    for (IPackageFragment tempPackage : packages) {
-	    	// If the package is located within a source root (K_SOURCE) the
-	    	// create a new AST for that package.
-	      if (tempPackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
-	        createAST(tempPackage);
-	      }
-	    }
-	  }
-	  /***
-	   * Takes one package fragment splits it up into its compilationUnits
-	   * which are source files.
-	   * compilationUnits (source files
-	   * @param tempPackage
-	   * @throws JavaModelException
-	   */
-	  private void createAST(IPackageFragment tempPackage)
-	      throws JavaModelException {
-	    
-		  // For each source file  accept visitor
-		  for (ICompilationUnit unit : tempPackage.getCompilationUnits()) {
-			  CompilationUnit tempCompilationUnit = parseCompilationUnit(unit);
-			  ListAllMethodsVisitor visitor = new ListAllMethodsVisitor();
-			  
-			  tempCompilationUnit.accept(visitor);
-			  List<MethodDeclaration> otherMethods = visitor.getOtherMethods();
-			  for (MethodDeclaration otherMethod : otherMethods)
-			  {
-				  System.out.println("Pattern: " + otherMethod.getName().getIdentifier());
-			  }
-			  // Get all tests in the selected package
-			  for (MethodDeclaration method : visitor.getTestMethods()) {
-				  	System.out.println("----- New test method -----");
-					  System.out.println("Method name: " + method.getName());
-					  List<Statement> rows = new ArrayList<Statement>();
-					  rows = method.getBody().statements();
-					 
-					  
-					  // Prints all rows in all test methods
-					  // TODO: parse out all method calls.
-					  // TODO: Must be some better way to identify method calls?
-					  for (Statement row: rows) 
-					  {
-						  System.out.println("----- New row -----");
-						  System.out.println("Statement=" + row);
-						  //for (MethodDeclaration otherMethod : otherMethods)
-						  for (int i = 1; i<5;i++)
-						  {
-							  System.out.println("----- New otherMethod -----");
-							  // System.out.println("Statement=" + row + " does it match=" + otherMethod.getName());
-							 /*
-							  if (method.getName().getIdentifier().matches(otherMethod.getName().getIdentifier()))
-							  {
-								  System.out.println("Statement:" + row + "Method: " + method.getName());
-							  }
-							  */
-						  }
-						  // For each statement regexp match against visitor.otherMethods()
-					  		// If otherMethods exists within statement remove it from visitor.otherMethods.
-						  // System.out.println(row);
-					  }
-				  
-				  // TODO: What is bindings? Expensive when creating AST below but what does it give me?
-				  IMethodBinding bindings = method.resolveBinding();
-				  // System.out.println("TEST: " + bindings.getName());
-			  }
-	    }
-	  }
-  
-	  private static CompilationUnit parseCompilationUnit(ICompilationUnit tempCompilationUnit) {
-		  // Updated to AST.JLS4
-		  ASTParser parser = ASTParser.newParser(AST.JLS4);
-		  
-		  parser.setKind(ASTParser.K_COMPILATION_UNIT);
-		  parser.setSource(tempCompilationUnit);
-		  
-		  // TODO: What does this add as functionality goes?
-		  // Warning, considerable cost in both time and space added.
-		  parser.setResolveBindings(true);
-		  
-		  return (CompilationUnit) parser.createAST(null); // parse
-	  }
+		for (IPackageFragment tempPackage : packages) {
+			// If the package is located within a source root (K_SOURCE) the
+			// create a new AST for that package.
+			if (tempPackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
+				createAST(tempPackage);
+			}
+		}
+	}
+	/***
+	 * Takes one package fragment splits it up into its compilationUnits
+	 * which are source files.
+	 * compilationUnits (source files
+	 * @param tempPackage
+	 * @throws JavaModelException
+	 */
+	private void createAST(IPackageFragment tempPackage)
+			throws JavaModelException {
+
+		// For each source file  accept visitor
+		for (ICompilationUnit unit : tempPackage.getCompilationUnits()) {
+			CompilationUnit tempCompilationUnit = parseCompilationUnit(unit);
+			ListAllMethodsVisitor visitor = new ListAllMethodsVisitor();
+
+			tempCompilationUnit.accept(visitor);
+			
+			// Get all tests in the selected package
+			for (MethodDeclaration method : visitor.getMethods()) {
+				// Parse all methods and group them.
+				if (method.getName().getIdentifier().matches(ListAllMethodsHandler.VALID_PATTERN_TESTS))
+					testMethods.add(method);
+				else
+					nonTestMethods.add(method);
+					
+				
+				
+				// TODO: What is bindings? Expensive when creating AST below but what does it give me?
+				IMethodBinding bindings = method.resolveBinding();
+			}
+		}
+		
+		for (MethodDeclaration testMethod : testMethods)
+		{
+			for (MethodDeclaration nonTestMethod : nonTestMethods)
+			{
+				List<Statement> statementList = new ArrayList<Statement>();
+				statementList = testMethod.getBody().statements();
+				
+				for (Statement st : statementList )
+				{
+					// Trim the string for each statement otherwise
+					// newline will disturb the .matches() regex function.
+					String tempString = new String(st.toString());
+					tempString = tempString.trim();
+					
+					// Pattern is any character in front and after the method name.
+					String pattern = ".*" + nonTestMethod.getName().getIdentifier() + ".*"; 
+
+					if (tempString.matches(pattern))
+					{
+						System.out.println("+++ Match: " + nonTestMethod.getName().getIdentifier() +
+								" executed in " + testMethod.getName().getIdentifier());
+					} else 
+					{
+						System.out.println("--- Nope");
+					}
+					
+				}
+			}
+		}
+	}
+
+	private static CompilationUnit parseCompilationUnit(ICompilationUnit tempCompilationUnit) {
+		// Updated to AST.JLS4
+		ASTParser parser = ASTParser.newParser(AST.JLS4);
+
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(tempCompilationUnit);
+
+		// TODO: What does this add as functionality goes?
+		// Warning, considerable cost in both time and space added.
+		parser.setResolveBindings(true);
+
+		return (CompilationUnit) parser.createAST(null); // parse
+	}
 } 
--- src/se/chho/tested/examples/ListAllMethodsVisitor.java
+++ src/se/chho/tested/examples/ListAllMethodsVisitor.java
@@ -7,36 +7,22 @@ import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 
 public class ListAllMethodsVisitor extends ASTVisitor {
-	
-	  private static final String VALID_PATTERN_TESTS = "^test(.*)";
-	  
-	  // Storage for all methods
-	  List<MethodDeclaration> testMethods = new ArrayList<MethodDeclaration>();
-	  List<MethodDeclaration> otherMethods = new ArrayList<MethodDeclaration>();
 
-	  // http://help.eclipse.org/juno/index.jsp?topic=%2Forg.eclipse.jdt.doc.isv%2Freference%2Fapi%2Forg%2Feclipse%2Fjdt%2Fcore%2Fdom%2Fpackage-summary.html&resultof=%22ASTVisitor%22%20%22astvisitor%22
-	  @Override
-	  public boolean visit(MethodDeclaration node) {
-		  if (node.getName().getIdentifier().matches(ListAllMethodsVisitor.VALID_PATTERN_TESTS))
-		  {
-			  testMethods.add(node);
-		  }
-		  else
-		  {
-			  System.out.println("*Visitor.java: " + node.getName());
-			  otherMethods.add(node);
-		  }
-	    return super.visit(node);
-	  }
-	  
-	  public List<MethodDeclaration> getTestMethods() {
-	    return testMethods;
-	  }
-	  
-	  public List<MethodDeclaration> getOtherMethods() {
-		  return otherMethods;
-	  }
-} 
+	// Storage for all methods
+	List<MethodDeclaration> methods = new ArrayList<MethodDeclaration>();
+
+	// http://help.eclipse.org/juno/index.jsp?topic=%2Forg.eclipse.jdt.doc.isv%2Freference%2Fapi%2Forg%2Feclipse%2Fjdt%2Fcore%2Fdom%2Fpackage-summary.html&resultof=%22ASTVisitor%22%20%22astvisitor%22
+	@Override
+	public boolean visit(MethodDeclaration node) {
+		methods.add(node);
+		return super.visit(node);
+	}
+
+	public List<MethodDeclaration> getMethods() {
+		return methods;
+	}
+
+}
 
 
 
