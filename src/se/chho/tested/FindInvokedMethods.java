adec5de4f120600314a1b1986a070fc91f6f70f4
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-04-19 10:43:41 +0200

    Simple markers now show in column next to line numbers and in the Problems view.

--- src/se/chho/tested/FindInvokedMethods.java
+++ src/se/chho/tested/FindInvokedMethods.java
@@ -59,50 +59,26 @@ public class FindInvokedMethods {
 		{
 			// Pass
 		}
-  
-		  // Test print out of this ArrayList
-//		  for (ITestCaseElement passed : this.passedTests)
-//		  {
-//			  System.out.println(passed.getTestMethodName());
-//		  }
-		  
+	  
 		  for (FoundMethod foundMethod : foundMethods)
 		  {
-			  if (foundMethod.isInvokedByMoreThanTwoTests())
-			  {
-				  
-//				try {		
-//					int linenumber = getMethodLineNumber(foundMethod.getMethod().getCompilationUnit(), foundMethod.getMethod());
-//					System.out.println("Line Number: " + linenumber);
-//					
-//					IMarker marker = foundMethod.getMethod().getCorrespondingResource().createMarker(IMarker.MARKER);
-//					marker.setAttribute(IMarker.LINE_NUMBER, linenumber);
-//					marker.setAttribute(IMarker.SEVERITY, IMarker.PRIORITY_NORMAL);
-//					marker.setAttribute("description", "test description");
-//					marker.setAttribute(IMarker.MESSAGE, "My marker message");
-//					
-//				} catch (JavaModelException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				} catch (CoreException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
-			  }
 			  String newLine = System.getProperty("line.separator");
 			  System.out.println(foundMethod.getName() + " found in " + foundMethod.getDiffTestMethods() + " different test functions.");
 			  System.out.println("Is " + foundMethod.getName() + " found in more than 2 test methods? " + foundMethod.isInvokedByMoreThanTwoTests() + newLine);
+			  
 			  // TODO: Figure out how to show marker for all methods
 			  if (foundMethod.isInvokedByMoreThanTwoTests())
 			  {
+				
 				  try {
-					  IResource m = foundMethod.getMethod().getCorrespondingResource();
+					  IResource m = foundMethod.getMethod().getCompilationUnit().getCorrespondingResource();
 					  int linenumber = getMethodLineNumber(foundMethod.getMethod().getCompilationUnit(), foundMethod.getMethod());
-					  IMarker marker = m.createMarker("org.eclipse.core.resources.problemmarker");
+					  IMarker marker = m.createMarker(IMarker.PROBLEM);
 					  marker.setAttribute(IMarker.LINE_NUMBER, linenumber);
 				  } catch (Exception e)
 				  {
 					  System.out.println(" ============= error ============= ");
+					  System.out.println(foundMethod.isInvokedByMoreThanTwoTests());
 					  e.printStackTrace();
 				  }
 			  }
@@ -173,6 +149,10 @@ public class FindInvokedMethods {
 	    return lineEnd.split(sourceUpToMethod).length;
 	}
 	
+	/***
+	 * For each nonTestMethod search in each testMethod.
+	 * If nonTestMethod is found in testMethod add to ArrayList<foundMethods>.
+	 */
 	private void populateFoundMethods()
 	{
 		// For each non test method, find if it's invoked in each test method one by one.
